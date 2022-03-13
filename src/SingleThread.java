890c87387961a2547b534aa22a15c967220f75f3
faridlamaul <lamaulfarid71@gmail.com>
2022-03-13 14:36:05 +0700

    update no-fix downloaded file

--- src/SingleThread.java
+++ src/SingleThread.java
@@ -1,19 +1,11 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,96 +21,65 @@ public class SingleThread {
             String message;
             String urn;
             String fileExtension;
-            String fileName;
             FileInputStream fis;
+            
             ServerSocket server = new ServerSocket(port);
-            while(true) {
-                System.out.println("------- Server started in port " + port + " -------");
-                Socket client = server.accept();
-                System.out.println("------- Client connected -------");
-                
-                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            System.out.println("------- Server started in port " + port + " -------");
+            Socket client = server.accept();
+            System.out.println("------- Client connected -------");
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 
-                message = br.readLine();
-                urn = message.split(" ")[1];
-                urn = urn.substring(1);
+            message = reader.readLine();
+            urn = message.split(" ")[1];
+            urn = urn.substring(1);
 
-                // get file extension
-                fileExtension = urn.substring(urn.lastIndexOf(".") + 1);
+            // get file extension
+            fileExtension = urn.substring(urn.lastIndexOf(".") + 1);
 
-                if(fileExtension.equals("html") || fileExtension.equals("txt")) {
-                    contentType = "text/html";
-                    try {
-                        fis = new FileInputStream(websiteRoot + urn);
-                        fileContent = new String(fis.readAllBytes());
-                        
-                        statusCode = "200 OK";
-                    } catch (FileNotFoundException e) {
-                        fileContent = "File not found";
-                        statusCode = "404 Not Found";
-                    }
+            if(fileExtension.equals("html") || fileExtension.equals("txt")) {
+                contentType = "text/html";
+                try {
+                    fis = new FileInputStream(websiteRoot + urn);
+                    fileContent = new String(fis.readAllBytes());
+                    
+                    statusCode = "200 OK";
+                } catch (FileNotFoundException e) {
+                    fileContent = "File not found";
+                    statusCode = "404 Not Found";
+                }
     
-                    while (!message.isEmpty()) {
-                        message = br.readLine();
-                    }
-        
-                    bw.write("HTTP/1.1 " + statusCode + crlf);
-                    bw.write("Content-Type: " + contentType + crlf);
-                    bw.write("Content-Length: " + fileContent.length() + crlf + crlf + fileContent);
-                    bw.flush();
-                    bw.close();
-                } else { 
-                    contentType = "application/octet-stream";
+                output.write("HTTP/1.1 " + statusCode + crlf);
+                output.write("Content-Type: " + contentType + crlf);
+                output.write("Content-Length: " + fileContent.length() + crlf + crlf + fileContent);
+                output.flush();
+                output.close();
 
-                    try {
-                        fis = new FileInputStream(websiteRoot + urn);
-                        fileContent = "This file is being downloaded (" + urn + ")";
-                        
-                        statusCode = "200 OK";
-                    } catch (FileNotFoundException e) {
-                        fileContent = "File not found";
-                        statusCode = "404 Not Found";
-                        
-                    }
-    
-                    while (!message.isEmpty()) {
-                        message = br.readLine();
-                    }
-        
-                    bw.write("HTTP/1.1 " + statusCode + crlf);
-                    System.out.println(statusCode);
-                    bw.write("Content-Type: " + contentType + crlf);
-                    System.out.println(contentType);
-                    bw.write("Content-Length: " + fileContent.length() + crlf + crlf + fileContent);
-                    // bw.write("Content-Disposition: attachment; filename=" + urn + crlf + crlf);
-                    System.out.println(urn);
-                    bw.flush();
-                    bw.close();
+            } else { 
+                // contentType = "application/octet-stream";
+                try {
+                    fis = new FileInputStream(websiteRoot + urn);
+                    fileContent = new String(fis.readAllBytes());
+                    
+                    statusCode = "200 OK";
+                } catch (FileNotFoundException e) {
+                    fileContent = "File not found";
+                    statusCode = "404 Not Found";
+                    
                 }
                 
-                
-                // try {
-                //     fis = new FileInputStream(websiteRoot + urn);
-                //     fileContent = new String(fis.readAllBytes());
-                    
-                //     statusCode = "200 OK";
-                // } catch (FileNotFoundException e) {
-                //     fileContent = "File not found";
-                //     statusCode = "404 Not Found";
-                // }
-
-                // while (!message.isEmpty()) {
-                //     // System.out.println(message);
-                //     message = br.readLine();
-                // }
-    
-                // bw.write("HTTP/1.1 " + statusCode + crlf + "Content-Type: " + contentType + crlf + "Content-Length: " + fileContent.length() + crlf + crlf + fileContent);
-                // bw.flush();
-    
-                client.close();
+                output.write("HTTP/1.1 " + statusCode + crlf);
+                output.write("Content-Type: text/html" + crlf);
+                // output.write("Content-Type: " + contentType + crlf);
+                output.write("Content-Length: " + fileContent.length() + crlf);
+                output.write("Content-Disposition: inline" + crlf + crlf + fileContent);
+                output.flush();
+                output.close();
             }
-                // server.close();
+
+            client.close();
+            server.close();
         } catch (Exception e) {
             Logger.getLogger(SingleThread.class.getName()).log(Level.SEVERE, null, e);
         }
